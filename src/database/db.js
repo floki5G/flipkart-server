9fdfd8890ab057f39776c0525b680da840161fbb
sameer kumar verma <83910505+floki5G@users.noreply.github.com>
2022-01-06 20:00:30 +0530

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -5,7 +5,12 @@ const Connection = async () => {
     
     try {
         const url = `mongodb+srv://root:${process.env.PASS}@flipkart-project.mwxvb.mongodb.net/${process.env.MYFIRST}?retryWrites=true&w=majority`;
-        await mongoose.connect(url);
+        await mongoose.connect(url,    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
         console.log('MongoDB connected');
     } catch (error){
         console.log("erroe db ")
